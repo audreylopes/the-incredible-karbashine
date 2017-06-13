49e29e762ff14f8496a30c3ff317b781700059b1
Miguel Ferreira <miguel.ferreira89@gmail.com>
2017-06-13 15:52:11 +0100

    even more gameobjectTypes

--- src/org/academiadecodigo/bootcamp/gameobjects/WindBlower.java
+++ src/org/academiadecodigo/bootcamp/gameobjects/WindBlower.java
@@ -1,7 +1,29 @@
 package org.academiadecodigo.bootcamp.gameobjects;
 
+import org.academiadecodigo.bootcamp.Pickable;
+import org.academiadecodigo.bootcamp.Representable;
+
 /**
  * Created by codecadet on 13/06/2017.
  */
-public class WindBlower {
+public class WindBlower implements Representable, Pickable {
+    @Override
+    public void move(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void show() {
+
+    }
 }
