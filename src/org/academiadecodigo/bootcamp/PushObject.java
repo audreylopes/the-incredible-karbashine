c644e8d51ac6ad0b2110961ff1d7cb45cc9703d3
Audrey Lopes <audrey_lopes@hotmail.com>
2017-06-13 20:26:41 +0100

    PushObject on progress

---
+++ src/org/academiadecodigo/bootcamp/PushObject.java
@@ -0,0 +1,46 @@
+package org.academiadecodigo.bootcamp;
+
+/**
+ * Created by codecadet on 13/06/17.
+ */
+public class PushObject extends GameObject implements Representable {
+
+
+    private Direction direction;
+
+
+    public PushObject(RepresentablePics picture) {
+        super(picture);
+    }
+
+
+    private boolean getCollision(){
+        
+
+    }
+
+
+    public void pushRight(){
+
+    }
+
+    public void pushLeft(){
+
+    }
+
+
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
--- src/org/academiadecodigo/bootcamp/gameobjects/WindBlower.java
+++ src/org/academiadecodigo/bootcamp/gameobjects/WindBlower.java
@@ -1,15 +1,12 @@
 package org.academiadecodigo.bootcamp.gameobjects;
 
-import org.academiadecodigo.bootcamp.GameObject;
-import org.academiadecodigo.bootcamp.Pickable;
-import org.academiadecodigo.bootcamp.Representable;
-import org.academiadecodigo.bootcamp.RepresentablePics;
+import org.academiadecodigo.bootcamp.*;
 import org.academiadecodigo.simplegraphics.mouse.MouseEvent;
 
 /**
  * Created by codecadet on 13/06/2017.
  */
-public class WindBlower extends GameObject implements Representable, Pickable {
+public class WindBlower extends PushObject implements Representable, Pickable {
 
     public WindBlower(RepresentablePics picture) {
         super(picture);
