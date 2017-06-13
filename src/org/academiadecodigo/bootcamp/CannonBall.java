46b1a190a62534c35a1c0a2bd3ba27e27785f800
Miguel Ferreira <miguel.ferreira89@gmail.com>
2017-06-13 15:25:41 +0100

    gameobjecttypes

---
+++ src/org/academiadecodigo/bootcamp/CannonBall.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.bootcamp;
+
+/**
+ * Created by codecadet on 13/06/2017.
+ */
+public class CannonBall {
+}
--- src/org/academiadecodigo/bootcamp/GameObjectTypes.java
+++ src/org/academiadecodigo/bootcamp/GameObjectTypes.java
@@ -4,8 +4,24 @@ package org.academiadecodigo.bootcamp;
  * Created by codecadet on 12/06/17.
  */
 public enum GameObjectTypes {
+
     BASKETBALL,
     BASEBALL,
     BOWLINGBALL,
+    CANNONBAL,
+    BALLOON,
+    RAMP,
+    BELT,
+    BRICKS,
+    PIPES,
+    BOXGLOVES,
+    GUN,
+    WINDBLOWER,
+    CANNON,
+    CANDLE,
+    CONVEYOR,
+    ELECTRICMOTOR,
+    SEESAW,
+    TRAMPOLINE
 
 }
