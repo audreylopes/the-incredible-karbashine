e89b26135c3b1284c51d6596942cce6abb1329dc
Audrey Lopes <audrey_lopes@hotmail.com>
2017-06-15 10:25:11 +0100

    pushobject conflict solved

--- src/org/academiadecodigo/bootcamp/PushObject.java
+++ src/org/academiadecodigo/bootcamp/PushObject.java
@@ -22,23 +22,14 @@ public class PushObject extends GameObject {
     }
 
 
-<<<<<<< HEAD
+
     private boolean getCollision(GameObject gameObject) {
 
         if (collisionChecker.checksCollision(gameObject)) {
             collision = true;
         }
-=======
-    private boolean getCollision(){
-<<<<<<< HEAD
-
-return false;
->>>>>>> 294bcbb061614a0e5859e1ded69ac2bee46c3d13
-=======
-        return false;
->>>>>>> 1cabc321b9a6389f5bab7aba4181acfaaf0f09ec
-
     }
+
 
 
     public void pushRight(GameObject gameObject, int x, int y) {
@@ -72,17 +63,10 @@ return false;
         }
     }
 
-<<<<<<< HEAD
 
 
 
-    @Override
-    public void move(MouseEvent mouseEvent) {
 
-    }
-
-=======
->>>>>>> 1cabc321b9a6389f5bab7aba4181acfaaf0f09ec
     @Override
     public void moveWhenPicked() {
 
